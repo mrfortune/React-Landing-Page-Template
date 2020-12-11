6ac2ab8edeeddcbb1bf2b6c45270f278e4019e85
mrfortune <rharris@worldshaker.com>
2020-12-11 16:43:36 -0500

    commit

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export class Services extends Component {
   render() {
