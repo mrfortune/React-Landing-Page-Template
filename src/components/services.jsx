922d05a10693d3122a12f0cd7249783970d95f09
mrfortune <rharris@worldshaker.com>
2020-12-20 14:20:30 -0500

    commit

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -9,8 +9,7 @@ export class Services extends Component {
           <div className="section-title">
             <h2>Our Services</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-              dapibus leonec.
+              Here is a list of the services and soltuons that we provide to our clients.
             </p>
           </div>
           <div className="row">
